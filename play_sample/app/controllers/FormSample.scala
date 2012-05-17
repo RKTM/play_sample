7cc327a83f94b0d4f000b62e09b4e195e480793b
RKTM <rrrkitamura@gmail.com>
2012-05-17 23:38:47 +0900

    簡単なｆormサンプル。

---
+++ play_sample/app/controllers/FormSample.scala
@@ -0,0 +1,62 @@
+package controllers
+
+import play.api.data.Forms._
+import play.api.data.Forms.mapping
+import play.api.data.Forms.number
+import play.api.data.Forms.text
+import play.api.data.validation.Constraints._
+import play.api.data.Form
+import play.api.mvc.Action
+import play.api.mvc.Controller
+import play.Logger
+
+object FormSample extends Controller {
+
+  //Formを定義
+  val userForm = Form(
+    mapping(
+      "name" -> text.verifying(nonEmpty),
+      "age" -> number)(User.apply)(User.unapply))
+
+  //Formを表示
+  def index = Action {
+    Ok(views.html.sample_form(userForm))
+  }
+
+  //submitされた時に呼び出される処理
+  def submit = Action { implicit request =>
+
+    //def bindFromRequest ()(implicit request: play.api.mvc.Request[_]): Form[T]
+    //リクエストのデータをformにバインドして、そのformが戻り値になる。
+    userForm.bindFromRequest.
+
+	    //def fold [R] (hasErrors: (Form[T]) ⇒ R, success: (T) ⇒ R): R
+	    //R：リザルト（？）型
+    	//hasErrors： エラーのあるformを処理する関数
+    	//success：サブミットが成功したformを処理する関数
+    	//returns：リザルトのR 
+    	fold(
+
+	        formWithErrors => {
+	          Logger.info("NG")
+	          //formでエラーがあった場合の処理
+	          
+	          BadRequest(views.html.sample_form(formWithErrors))
+	        },
+	        userForm => {
+	          Logger.info("OK")
+	          //ここで入力が正常に行われた場合の処理を行う。
+	
+	          //例：DB更新
+	
+	          //処理が終わったらリダイレクト
+	          Redirect(routes.FormSample.show(userForm.name, userForm.age))
+	        })
+  }
+
+  //submitして正常終了した時にRedirectで呼び出される処理
+  def show(name: String, age: Int) = Action { implicit requst =>
+    Ok(views.html.sample_form_show(name, age))
+  }
+
+}
---
+++ play_sample/app/controllers/Forms.scala
@@ -0,0 +1,3 @@
+package controllers
+
+case class User(name: String, age: Int)
